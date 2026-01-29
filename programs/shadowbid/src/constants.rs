8b9a0308c68c8fcfccdd56fc6b021568c4792feb
Ge0frey <jeffkimzyjoe@gmail.com>
2026-01-29 07:37:05 +0300

    Program constants

---
+++ programs/shadowbid/src/constants.rs
@@ -0,0 +1,23 @@
+use anchor_lang::prelude::*;
+
+/// Seed for deriving Auction PDA
+pub const AUCTION_SEED: &[u8] = b"auction";
+
+/// Seed for deriving Bid PDA
+pub const BID_SEED: &[u8] = b"bid";
+
+/// Minimum auction duration (1 hour in seconds)
+pub const MIN_AUCTION_DURATION: i64 = 3600;
+
+/// Maximum auction duration (7 days in seconds)
+pub const MAX_AUCTION_DURATION: i64 = 604800;
+
+/// Maximum title length in bytes
+pub const MAX_TITLE_LENGTH: usize = 64;
+
+/// Maximum description length in bytes
+pub const MAX_DESCRIPTION_LENGTH: usize = 256;
+
+/// Inco Lightning Program ID
+pub const INCO_LIGHTNING_PROGRAM_ID: Pubkey = 
+    pubkey!("5sjEbPiqgZrYwR31ahR6Uk9wf5awoX61YGg7jExQSwaj");
